7234a221587a875b383b0bf4c5bd79ea63707ab9
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-28 18:41:25 -0300

    :pencil2: fix(entities): Add producerId field

--- backend/internal/entities/affiliate.go
+++ backend/internal/entities/affiliate.go
@@ -2,6 +2,7 @@ package entities
 
 // Affiliate presents a affiliate content producer
 type Affiliate struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	ProducerId int64  `json:"producer_id"`
 }
--- backend/internal/entities/product.go
+++ backend/internal/entities/product.go
@@ -2,6 +2,7 @@ package entities
 
 // Product presents a course product
 type Product struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	ProducerId int64  `json:"producer_id"`
 }
