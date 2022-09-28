f3dca26a069c8cb0581f4ac601789e9a85cdbadc
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-28 15:32:38 -0300

    :sparkles: feat(product-repository): Add add-product-repository

---
+++ backend/internal/repositories/producer-repository/addproduct-repository.go
@@ -0,0 +1,16 @@
+package producer_repository
+
+import (
+	"context"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/customerror"
+)
+
+// AddProduct adds a new product in database and return the id
+func (r *productRepository) AddProduct(ctx context.Context, name string) (*int64, *customerror.CustomError) {
+	var id int64
+	err := r.sqlx.GetContext(ctx, &id, "INSERT INTO products(name) VALUES($1) RETURNING id", name)
+	if err != nil {
+		return nil, customerror.NewError(customerror.EINVALID, "Error", "producer_repository.GetAll", err)
+	}
+	return &id, nil
+}
---
+++ backend/internal/repositories/producer-repository/product-repository.go
@@ -0,0 +1,24 @@
+package producer_repository
+
+import (
+	"context"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/customerror"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/db"
+	"github.com/jmoiron/sqlx"
+)
+
+// IProductRepository presents the interface for the product repository
+type IProductRepository interface {
+	AddProduct(ctx context.Context, name string) (*int64, *customerror.CustomError)
+}
+
+type productRepository struct {
+	sqlx *sqlx.DB
+}
+
+// New creates a new product repository
+func New() IProductRepository {
+	return &productRepository{
+		db.ConnectDB(),
+	}
+}
