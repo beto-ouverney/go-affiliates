3e4d75e4f279e1ac035cd0bf625d2e0b26cc2494
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-28 19:06:36 -0300

    :sparkles: feat(sale-repository): Add sale repository

---
+++ backend/internal/repositories/sale-repository/sale-repository.go
@@ -0,0 +1,25 @@
+package sale_repository
+
+import (
+	"context"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/customerror"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/db"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/entities"
+	"github.com/jmoiron/sqlx"
+)
+
+// ISaleRepository presents the interface for the sales repository
+type ISaleRepository interface {
+	Add(ctx context.Context, sale []entities.Sale) *customerror.CustomError
+}
+
+type saleRepository struct {
+	sqlx *sqlx.DB
+}
+
+// New creates a new sales repository
+func New() ISaleRepository {
+	return &saleRepository{
+		db.ConnectDB(),
+	}
+}
