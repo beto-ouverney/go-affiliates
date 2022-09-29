7c728048def8061c7e3a575ad6394d8f52068826
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-29 00:44:48 -0300

    fix(entities): Fix fields types of the DataEntry entity

--- backend/internal/entities/data-entry.go
+++ backend/internal/entities/data-entry.go
@@ -2,8 +2,8 @@ package entities
 
 // DataEntry presents a data line in the data file
 type DataEntry struct {
-	Type    string `json:"type"`
-	Data    string `json:"data"`
+	Type    int    `json:"type"`
+	Date    string `json:"date"`
 	Product string `json:"product"`
 	Value   int    `json:"value"`
 	Seller  string `json:"seller"`
