defc1771eba3225eacd977e613aa85552ed19bc8
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-30 20:24:53 -0300

    :sparkles: feat(sales-usecase): Add removeDuplicate

---
+++ backend/internal/usecases/sales-usecase/removeduplicate.go
@@ -0,0 +1,21 @@
+package sales_usecase
+
+import "github.com/beto-ouverney/go-affiliates/backend/internal/entities"
+
+// typeSet for Generics in removeDuplicates
+type typeSet interface {
+	entities.Producer | entities.Product
+}
+
+// removeDuplicate removes duplicate strings or struct from a slice
+func removeDuplicate[T typeSet](sliceList []T) []T {
+	allKeys := make(map[T]bool)
+	var list []T
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
