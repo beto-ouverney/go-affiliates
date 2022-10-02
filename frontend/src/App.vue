229c517a40e01a941050f11232f437bf2c634872
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-10-02 19:12:22 -0300

    :tada: build(frontend): init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style lang="scss">
+nav {
+  display: flex;
+  flex-direction: row;
+  height: 5%;
+  justify-content: center;
+
+  a {
+    color: black;
+    font-weight: bold;
+    margin: 5px;
+    padding: 10px;
+
+    &.router-link-exact-active {
+      border: 1px solid rgb(8, 8, 209);
+      border-radius: 10px;
+      color: rgb(8, 8, 209);
+    }
+  }
+}
+</style>
