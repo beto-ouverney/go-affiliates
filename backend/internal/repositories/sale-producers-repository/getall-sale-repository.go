04607abc164fd129ec1d878df00bb635950372b2
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-10-02 09:53:33 -0300

    :sparkles: feat(sale-repository): Add get all sales producders with products

---
+++ backend/internal/repositories/sale-producers-repository/getall-sale-repository.go
@@ -0,0 +1,22 @@
+package sale_producers_repository
+
+import (
+	"context"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/customerror"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/entities"
+)
+
+// GetAll returns all sales producers with name of product, producer and affiliate
+func (r *saleRepository) GetAll(ctx context.Context) (*[]entities.SaleResponse, *customerror.CustomError) {
+
+	q := `SELECT sales_producers.value, sales_producers.commission, sales_producers.date, producers.name as "producer", products.name as "product" FROM sales_producers INNER JOIN producers ON sales_producers.producer_id = producers.id INNER JOIN products ON sales_producers.product_id = products.id`
+
+	sales := []entities.SaleResponse{}
+
+	err := r.sqlx.SelectContext(ctx, &sales, q)
+	if err != nil {
+		return nil, customerror.NewError(customerror.EINVALID, "Error", "sale_repository.GetAll", err)
+	}
+
+	return &sales, nil
+}
--- backend/internal/repositories/sale-producers-repository/sale-repository.go
+++ backend/internal/repositories/sale-producers-repository/sale-repository.go
@@ -11,6 +11,7 @@ import (
 // ISaleRepository presents the interface for the sales repository
 type ISaleRepository interface {
 	Add(ctx context.Context, s []entities.Sale) *customerror.CustomError
+	GetAll(ctx context.Context) (*[]entities.SaleResponse, *customerror.CustomError)
 }
 
 type saleRepository struct {
