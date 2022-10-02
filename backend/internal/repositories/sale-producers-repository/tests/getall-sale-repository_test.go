e38339a76dac70c733fbb77c526960c46406f9ed
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-10-02 09:59:02 -0300

    test(sale-producers-repository): add test for get all sales

---
+++ backend/internal/repositories/sale-producers-repository/tests/getall-sale-repository_test.go
@@ -0,0 +1,62 @@
+package tests
+
+import (
+	"context"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/customerror"
+	"github.com/beto-ouverney/go-affiliates/backend/internal/entities"
+	mocks "github.com/beto-ouverney/go-affiliates/backend/internal/repositories/sale-producers-repository/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func Test_saleRepository_GetAll(t *testing.T) {
+	assertions := assert.New(t)
+
+	mockSales := []entities.SaleResponse{
+		{
+			Producer:   "Producer 1",
+			Product:    "Product 1",
+			Value:      1000,
+			Commission: 0,
+			Date:       "22020-02-03T20:51:59-03:00",
+		},
+		{
+			Producer:   "Producer 2",
+			Product:    "Product 2",
+			Value:      2000,
+			Commission: 0,
+			Date:       "22022-02-03T20:51:59-03:00",
+		},
+	}
+
+	tests := []struct {
+		describe string
+		args     []entities.SaleResponse
+		want     *[]entities.SaleResponse
+		want1    *customerror.CustomError
+		msg      string
+		msg1     string
+	}{
+		{
+			describe: "Should be able get all sales with products, sales and affiliates",
+			args:     mockSales,
+			want:     &mockSales,
+			want1:    nil,
+			msg:      "Must be equal",
+			msg1:     "The error should be nil",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.describe, func(t *testing.T) {
+			ctx := context.Background()
+			m := new(mocks.ISaleRepository)
+			m.On("GetAll", mock.AnythingOfType("*context.emptyCtx")).Return(tt.want, tt.want1)
+
+			got, got1 := m.GetAll(ctx)
+			assertions.Equal(tt.want1, got1, tt.msg1)
+			assertions.EqualValues(tt.want, got, tt.msg)
+
+		})
+	}
+}
