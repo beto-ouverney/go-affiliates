7b022ef05c98aabb08993c55106a4b581b8ba90f
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-10-03 13:36:50 -0300

    :sparkles: feat(pages): add home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,36 @@
+<style lang="scss" scoped>
+@import "./styles/HomeView.scss";
+</style>
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="top-home">
+    <h1>Go Affiliates</h1>
+    <div class="navigation">
+      <SendFile />
+      <FilterComponent />
+    </div>
+    <TableSales :show="showTable" />
+    <LoadingComponent :show="isLoading" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import FilterComponent from "@/components/home/FilterComponent.vue";
+import SendFile from "@/components/home/SendFile.vue";
+import TableSales from "@/components/home/TableSales.vue";
+import { mapState } from "vuex";
+import LoadingComponent from "@/components/shared/LoadingComponent.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    FilterComponent,
+    SendFile,
+    LoadingComponent,
+    TableSales,
+  },
+  computed: {
+    ...mapState(["isLoading", "showTable"]),
   },
 });
 </script>
